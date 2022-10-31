2c07184ca2bd683df62939e8fa4db75b526bfd04
Дмитрий Ерыганов <dnevvs@yandex.ru>
2022-10-31 19:02:13 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница Django')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'И зачем эта страница {slug}?')
+
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+# def ice_cream_detail(request, pk):
+#    return HttpResponse(f'Мороженое номер {pk}')
